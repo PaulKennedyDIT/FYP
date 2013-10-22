d3b2b40ea300190fa434f9b750c2c9538e1062bd
PaulKennedyDIT <c10319445@mydit.ie>
2013-10-22 18:32:32 +0100

    Version 0.3.5

--- FluidDynamicsEquations/BaseEQ.cpp
+++ FluidDynamicsEquations/BaseEQ.cpp
@@ -14,9 +14,7 @@ BaseEQ::~BaseEQ(void)
 
 /*
 Bernoulli Equation to solve the headloss given the following criteria:
-Headloss = Elevation 1 - Elevation 2 +	(Position 1 - Position 2)			+ Velocity1^2 - Velocity2^2
-									 ________________________________		_______________________
-									density * Acceleration of gravity			2 * Acceleration of Gravity
+Headloss = Elevation 1 - Elevation 2 +	(Position 1 - Position 2)/(density * Acceleration of gravity)			+ Velocity1^2 - Velocity2^2/ (2 * Acceleration of Gravity)												
 */
 float BaseEQ::BernoulliHeadLoss(glm::vec2 elevation, glm::vec2 pressure,glm::vec2 velocity,float density,float aOG)
 {
@@ -27,9 +25,7 @@ float BaseEQ::BernoulliHeadLoss(glm::vec2 elevation, glm::vec2 pressure,glm::vec
 
 /*
 Bernoulli Equation to solve for an elevation at point 1:
-Elevation 1 =  Elevation 2 +	(Position 2 - Position 1)			+	Velocity1^2 - Velocity2^2
-							 ________________________________			_______________________			+ Headloss
-							density * Acceleration of gravity			2 * Acceleration of Gravity
+Elevation 1 =  Elevation 2 +	(Position 2 - Position 1)/(density * Acceleration of gravity)			+	Velocity1^2 - Velocity2^2/(2 * Acceleration of Gravity) + Headloss
 */
 float BaseEQ::BernoulliElevation(glm::vec2 elevation, glm::vec2 pressure, glm::vec2 velocity, float density, float aOG,float headloss)
 {
@@ -39,9 +35,7 @@ float BaseEQ::BernoulliElevation(glm::vec2 elevation, glm::vec2 pressure, glm::v
 
 /*
 Bernoulli Equation to solve for pressure at point 1:
-Pressure 1 =  Density * Acceleration of gravity  * 	(Position 2 - Position 1  + Velocity1^2 - Velocity2^2		+ headloss)
-																				_______________________	
-																				2 * Acceleration of Gravity
+Pressure 1 =  Density * Acceleration of gravity  * 	(Position 2 - Position 1  + (Velocity1^2 - Velocity2^2) / (2 * Acceleration of Gravity)	+ headloss)																				
 */
 float BaseEQ::BernoulliPressure(glm::vec2 elevation, glm::vec2 pressure, glm::vec2 velocity, float density, float aOG,float headloss)
 {
@@ -51,9 +45,7 @@ float BaseEQ::BernoulliPressure(glm::vec2 elevation, glm::vec2 pressure, glm::ve
 
 /*
 Bernoulli Equation to solve for velocity at point 1:
-Velocity 1 =  Square Root (2 * Acceleration of gravity  * 	((Position 2 - Position 1)			+	Velocity1^2 - Velocity2^2			+	Velocity 2 ^2				+		Headloss
-																										_______________________				______________
-																										2 * Acceleration of Gravity		2 * Acceleration of Gravity
+Velocity 1 =  Square Root (2 * Acceleration of gravity * ((Position 2 - Position 1)	+	(Velocity1^2 - Velocity2^2m)/(2 * Acceleration of Gravity) + (Velocity 2 ^2) / (2 * Acceleration of Gravity) + Headloss																												
 */
 float BaseEQ::BernoulliVelocity(glm::vec2 elevation, glm::vec2 pressure, glm::vec2 velocity, float density, float aOG,float headloss)
 {
